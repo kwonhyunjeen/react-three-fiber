68270c65d9234a7f2addfcce4e881676d7eb2283
Hyunjin Kwon <kwonhyunjeen@gmail.com>
2024-12-14 15:03:23 +0900

    set up tailwindcss and tanstack router to project

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <>R3F Basic</>;
-}
-
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto Mono", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
